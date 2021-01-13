0fdf98ca873260024b778acef55ba676adc329c5
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-01-12 23:25:27 -0800

    Added styling and moved daily route to its own page

--- index.js
+++ index.js
@@ -4,22 +4,45 @@ const path = require('path');
 const axios = require('axios').default;
 const ejsMate = require('ejs-mate');
 
-app.get('/', async (req, res) => {
-  let daily;
-  let quests;
-  let questIds = [];
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/daily', async (req, res) => {
+  let daily,
+      quests,
+      questIds = [],
+      tomorrowQuests,
+      tomorrowIds = [];
   try {
-    const response = await axios.get('https://api.guildwars2.com/v2/achievements/daily')
-    for (let questId of response.data.pve) {
+    // Current daily achievements
+    const currentResponse = await axios.get('https://api.guildwars2.com/v2/achievements/daily')
+
+    // Add all daily IDs to questIds array
+    for (let questId of currentResponse.data.pve) {
       questIds.push(questId.id);
     }
+
+    // Get daily names from IDs
     daily = await axios.get(`https://api.guildwars2.com/v2/achievements?ids=${questIds.join()}`)
     quests = daily.data;
-    console.log(quests[0].tiers.count)
+
+    // Tomorrow's daily achievements
+    const tomorrowResponse = await axios.get('https://api.guildwars2.com/v2/achievements/daily/tomorrow')
+    
+    // Add daily IDs to tomorrowIds array
+    for (let questId of tomorrowResponse.data.pve) {
+      tomorrowIds.push(questId.id);
+    }
+
+    // Get daily names from IDs
+    tomorrow = await axios.get(`https://api.guildwars2.com/v2/achievements?ids=${tomorrowIds.join()}`)
+    tomorrowQuests = tomorrow.data;
+    
   } catch (err) {
     console.log(err)
   }
-  res.render('index', { quests });
+  res.render('daily', { quests, tomorrowQuests });
 });
 
 app.engine('ejs', ejsMate);
