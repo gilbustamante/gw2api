892d9ac556288db48305ba27db13f4a0dbfead98
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-01-15 22:38:41 -0800

    Shorten variable names passed to daily view

--- controllers/daily.js
+++ controllers/daily.js
@@ -1,85 +1,27 @@
 const axios = require('axios').default;
 const filterAchievements = require('../utils/filterAchievements');
 
-// Make API Call And Display
-// module.exports.displayDailies = async (req, res) => {
-//   let daily,
-//       quests = [],
-//       questIds = [],
-//       tomorrowQuests = [],
-//       tomorrowIds = [];
-//   try {
-//     // Current daily achievements
-//     const currentResponse = await axios.get('https://api.guildwars2.com/v2/achievements/daily?v=2019-05-16T00:00:00.000Z')
-
-//     // Add all daily IDs to questIds array
-//     for (let questId of currentResponse.data.pve) {
-//       // If it's a low level quest, ignore
-//       if (questId.level.max < 80) { continue; }
-
-//       // If it's meant for non PoF users, ignore
-//       if (questId.required_access) {
-//         if (questId.required_access.condition === 'NoAccess') {
-//           continue;
-//         } else {
-//           questIds.push(questId.id);
-//           continue;
-//         }
-//       }
-//       questIds.push(questId.id);
-//     }
-
-//     // Get daily names from IDs
-//     daily = await axios.get(`https://api.guildwars2.com/v2/achievements?ids=${questIds.join()}`)
-//     quests = daily.data;
-
-//     // Tomorrow's daily achievements
-//     const tomorrowResponse = await axios.get('https://api.guildwars2.com/v2/achievements/daily/tomorrow?v=2019-05-16T00:00:00.000Z')
-    
-//     // Add all daily IDs to questIds array
-//     for (let tomorrowId of tomorrowResponse.data.pve) {
-//       // If it's a low level quest, ignore
-//       if (tomorrowId.level.max < 80) { continue; }
-
-//       // If it's meant for non PoF users, ignore
-//       if (tomorrowId.required_access) {
-//         if (tomorrowId.required_access.condition === 'NoAccess') {
-//           continue;
-//         } else {
-//           tomorrowIds.push(tomorrowId.id);
-//           continue;
-//         }
-//       }
-//       tomorrowIds.push(tomorrowId.id);
-//     }
-
-//     // Get daily names from IDs
-//     tomorrow = await axios.get(`https://api.guildwars2.com/v2/achievements?ids=${tomorrowIds.join()}`)
-//     tomorrowQuests = tomorrow.data;
-
-//   } catch (err) {
-//     console.log(err)
-//   }
-//   res.render('daily', { quests, tomorrowQuests });
-// };
-
-// Globals
+//// Globals
 // Today
 let todayAchievements = {};
-let todayCategories = {};
+let todayCats = {};
 let todayDailies = {};
 // Tomorrow
 let tomorrowAchievements = {};
-let tomorrowCategories = {};
+let tomorrowCats = {};
 let tomorrowDailies = {};
+
+// Today's Quests Endpoint
 const dailyUrl = 'https://api.guildwars2.com/v2/achievements/daily?v=2019-05-16T00:00:00.000Z';
+// Tomorrow's Quests Endpoint
 const dailyTomorrowUrl = 'https://api.guildwars2.com/v2/achievements/daily/tomorrow?v=2019-05-16T00:00:00.000Z'
+// Lookup Quest IDs Endpoint
 const dailyLookupUrl = 'https://api.guildwars2.com/v2/achievements?ids='
 
 module.exports.dailyBeta = async (req, res) => {
   let todayBuffer = [];
   let tomorrowBuffer = [];
-  todayCategories = {
+  todayCats = {
     'pve': [],
     'pvp': [],
     'wvw': [],
@@ -87,7 +29,7 @@ module.exports.dailyBeta = async (req, res) => {
     'special': []
   }
 
-  tomorrowCategories = {
+  tomorrowCats = {
     'pve': [],
     'pvp': [],
     'wvw': [],
@@ -95,6 +37,7 @@ module.exports.dailyBeta = async (req, res) => {
     'special': []
   }
 
+  //// TODO: Refactor this stuff ////
   try {
     // Request Today's Dailies
     const today = await axios.get(dailyUrl);
@@ -109,32 +52,32 @@ module.exports.dailyBeta = async (req, res) => {
       const id = filterAchievements(achievement)
       if (id) {
         todayBuffer.push(id)
-        todayCategories.pve.push(id)
+        todayCats.pve.push(id)
       }
     }
 
     // Today - PvP
     for (let achievement of todayDailies.pvp) {
       todayBuffer.push(achievement.id)
-      todayCategories.pvp.push(achievement.id)
+      todayCats.pvp.push(achievement.id)
     }
 
     // Today - WvW
     for (let achievement of todayDailies.wvw) {
       todayBuffer.push(achievement.id)
-      todayCategories.wvw.push(achievement.id)
+      todayCats.wvw.push(achievement.id)
     }
 
     // Today - Fractals
     for (let achievement of todayDailies.fractals) {
       todayBuffer.push(achievement.id)
-      todayCategories.fractals.push(achievement.id)
+      todayCats.fractals.push(achievement.id)
     }
 
     // Today - Special Events
     for (let achievement of todayDailies.special) {
       todayBuffer.push(achievement.id)
-      todayCategories.special.push(achievement.id)
+      todayCats.special.push(achievement.id)
     }
 
     // Tomorrow - PvE
@@ -142,32 +85,34 @@ module.exports.dailyBeta = async (req, res) => {
       const id = filterAchievements(achievement)
       if (id) {
         tomorrowBuffer.push(id)
-        tomorrowCategories.pve.push(id)
+        tomorrowCats.pve.push(id)
       }
     }
 
-    // Today - PvP
+    //////////////////////////////////////////////
+
+    // Tomorrow - PvP
     for (let achievement of tomorrowDailies.pvp) {
       tomorrowBuffer.push(achievement.id)
-      tomorrowCategories.pvp.push(achievement.id)
+      tomorrowCats.pvp.push(achievement.id)
     }
 
-    // Today - WvW
+    // Tomorrow - WvW
     for (let achievement of tomorrowDailies.wvw) {
       tomorrowBuffer.push(achievement.id)
-      tomorrowCategories.wvw.push(achievement.id)
+      tomorrowCats.wvw.push(achievement.id)
     }
 
-    // Today - Fractals
+    // Tomorrow - Fractals
     for (let achievement of tomorrowDailies.fractals) {
       tomorrowBuffer.push(achievement.id)
-      tomorrowCategories.fractals.push(achievement.id)
+      tomorrowCats.fractals.push(achievement.id)
     }
 
-    // Today - Special Events
+    // Tomorrow - Special Events
     for (let achievement of tomorrowDailies.special) {
       tomorrowBuffer.push(achievement.id)
-      tomorrowCategories.special.push(achievement.id)
+      tomorrowCats.special.push(achievement.id)
     }
 
     // Lookup Achievement IDs
@@ -179,25 +124,25 @@ module.exports.dailyBeta = async (req, res) => {
     todayAchievements = todayDetails.data;
     tomorrowAchievements = tomorrowDetails.data;
 
-    todayAchievementsDict = {};
+    todayDict = {};
     for (let achievement of todayAchievements) {
       const achievementID = achievement.id;
-      todayAchievementsDict[achievementID] = achievement;
+      todayDict[achievementID] = achievement;
     }
 
-    tomorrowAchievementsDict = {};
+    tomorrowDict = {};
     for (let achievement of tomorrowAchievements) {
       const achievementID = achievement.id;
-      tomorrowAchievementsDict[achievementID] = achievement;
+      tomorrowDict[achievementID] = achievement;
     }
 
   } catch (err) {
     console.log(err);
   }
   res.render('daily', {
-    todayAchievementsDict,
-    tomorrowAchievementsDict,
-    todayCategories,
-    tomorrowCategories
+    todayDict,
+    tomorrowDict,
+    todayCats,
+    tomorrowCats
   });
 }
