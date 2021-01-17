9242922a1ff8fd67ff596a71c7645df3755272e7
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-01-16 20:15:50 -0800

    Market controller correctly looks up item info

--- controllers/market.js
+++ controllers/market.js
@@ -2,17 +2,35 @@ const axios = require('axios').default;
 
 module.exports.renderMarket = async (req, res) => {
   try {
+    var itemBuffer = [];
+    var itemsDict = {};
+    var items = [];
+
     const config = {
       headers: {
         Authorization: 'Bearer ' + process.env.API_KEY
       }
     }
-    const url = 'https://api.guildwars2.com/v2/commerce/transactions/history/sells';
-    const res = await axios.get(url, config)
-    var items = res.data;
+    const marketUrl = 'https://api.guildwars2.com/v2/commerce/transactions/history/sells';
+    const marketRes = await axios.get(marketUrl, config)
+
+    items = marketRes.data;
+
+    for (let i of marketRes.data) {
+      itemBuffer.push(i.item_id)
+    }
+
+    const itemUrl = 'https://api.guildwars2.com/v2/items?ids='
+    const itemRes = await axios.get(itemUrl + itemBuffer.join())
+
+    for (let i of itemRes.data) {
+      const id = i.id;
+      itemsDict[id] = i;
+    }
+
   } catch (err) {
     console.log(err)
   }
 
-  res.render('market', { items });
+  res.render('market', { items, itemsDict });
 }
