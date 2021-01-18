e83fce9a7048666a08f73e649c3997f0fb9b11d6
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-01-18 15:47:07 -0800

    API post route now clears previous cookie

--- controllers/api.js
+++ controllers/api.js
@@ -6,6 +6,9 @@ module.exports.renderAPIForm = (req, res) => {
 
 // Handle API Key
 module.exports.handleAPI = async (req, res) => {
+  // Clear current (if any) apiKey cookie
+  res.clearCookie('apiKey');
+
   // Cookie config
   const config = {
     signed: true,
@@ -13,12 +16,14 @@ module.exports.handleAPI = async (req, res) => {
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
-  // Set cookie
+
+  // Set new cookie
   res.cookie('apiKey', req.body.apiKey, config)
+
   // If user was redirected here from hasCookie middleware, redirect back
   // otherwise redirect to index
   const redirectUrl = req.session.returnTo || '/';
   delete req.session.returnTo; // Clear returnTo from session
-  req.flash('success', 'API key added.')
+  req.flash('success', 'API key added (expires in one week).')
   res.redirect(redirectUrl);
 }
