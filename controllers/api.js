1e13a86b59ac99d0d40fa3284e2d583d5fd48577
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-01-18 11:43:41 -0800

    Set config options for api cookie

--- controllers/api.js
+++ controllers/api.js
@@ -6,11 +6,19 @@ module.exports.renderAPIForm = (req, res) => {
 
 // Handle API Key
 module.exports.handleAPI = async (req, res) => {
+  // Cookie config
+  const config = {
+    signed: true,
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
   // Set cookie
-  res.cookie('apiKey', req.body.apiKey, { signed: true })
+  res.cookie('apiKey', req.body.apiKey, config)
   // If user was redirected here from hasCookie middleware, redirect back
   // otherwise redirect to index
   const redirectUrl = req.session.returnTo || '/';
+  delete req.session.returnTo; // Clear returnTo from session
   req.flash('success', 'API key added.')
   res.redirect(redirectUrl);
 }
