98770542caec326965790ec58d821d6e94f6fb1e
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-01-19 14:21:29 -0800

    Add currency images to market price view

--- public/js/convertGold.js
+++ public/js/convertGold.js
@@ -4,11 +4,14 @@ module.exports.convertGold = (amount) => {
     const copper = price.slice(-2); // last 2 digits
     const silver = price.slice(-4, -2); // next 2
     const gold   = price.slice(0, -4); // everything else
-    return `${gold} gold, ${silver} silver, ${copper} copper`;
+    return `${gold} <img src="/img/gold.png"> 
+            ${silver} <img src="/img/silver.png"> 
+            ${copper} <img src="/img/copper.png">`;
   } else if (price.length <= 4 && price.length > 2) {
     const copper = price.slice(-2);
     const silver = price.slice(0, -2);
-    return `${silver} silver, ${copper} copper`;
+    return `${silver} <img src="/img/silver.png"> 
+            ${copper} <img src="/img/copper.png">`;
   }
   return `${price} copper`;
 }
