78043e9e825c02a24b26acf298349c9d08200351
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-01-25 13:40:51 -0800

    Add hasCookie middleware to bank route

--- routes/bank.js
+++ routes/bank.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bank = require('../controllers/bank');
 const catchAsync = require('../utils/catchAsync');
+const { hasCookie } = require('../middleware');
 const router = express.Router();
 
 router.route('/')
-  .get(catchAsync(bank.renderBankInfo))
+  .get(hasCookie, catchAsync(bank.renderBankInfo))
 
 module.exports = router;
